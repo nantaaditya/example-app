01526120f729caa86f6994e6ff7a456522d9225e
nantaaditya <pramuditza@gmail.com>
2024-11-14 11:05:27 +0700

    chore: change how to use kafka subscriber

--- core/src/main/java/com/example/app/core/api/InternalAuditController.java
+++ core/src/main/java/com/example/app/core/api/InternalAuditController.java
@@ -3,7 +3,7 @@ package com.example.app.core.api;
 import com.example.app.core.utils.BalanceAuditService;
 import com.nantaaditya.framework.audit.model.request.GetChangelogRequest;
 import com.nantaaditya.framework.audit.model.response.GetChangelogResponse;
-import com.nantaaditya.framework.rest.model.Response;
+import com.nantaaditya.framework.helper.model.Response;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
--- core/src/main/java/com/example/app/core/subscriber/CreateBalanceSubscriber.java
+++ core/src/main/java/com/example/app/core/subscriber/CreateBalanceSubscriber.java
@@ -6,13 +6,15 @@ import com.example.app.shared.model.kafka.KafkaTopic;
 import com.nantaaditya.framework.helper.json.JsonHelper;
 import com.nantaaditya.framework.kafka.filter.IdempotentEventFilter;
 import com.nantaaditya.framework.kafka.helper.KafkaUtil;
+import com.nantaaditya.framework.kafka.model.constant.EventType;
 import com.nantaaditya.framework.kafka.properties.KafkaProperties;
+import com.nantaaditya.framework.kafka.service.BaseSubscriberServiceBuilder;
+import com.nantaaditya.framework.kafka.service.EventHandler;
 import com.nantaaditya.framework.kafka.service.SubscriberService;
 import com.nantaaditya.framework.metric.model.CounterMetric;
 import com.nantaaditya.framework.metric.model.TimerMetric;
 import com.nantaaditya.framework.reactor.api.SchedulerHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.ReceiverRecord;
@@ -21,20 +23,19 @@ import reactor.kafka.receiver.ReceiverRecord;
 @Component
 public class CreateBalanceSubscriber extends SubscriberService {
 
-  @Autowired
-  private BalanceRepository balanceRepository;
-
-  @Autowired
-  private JsonHelper jsonHelper;
-
   protected CreateBalanceSubscriber(
-      KafkaProperties kafkaProperties,
-      IdempotentEventFilter idempotentEventFilter,
-      SchedulerHelper schedulerHelper,
-      CounterMetric kafkaCounterMetric,
-      TimerMetric kafkaTimerMetric,
-      KafkaUtil kafkaUtil) {
-    super(kafkaProperties, idempotentEventFilter, schedulerHelper, kafkaCounterMetric, kafkaTimerMetric, kafkaUtil);
+      KafkaProperties kafkaProperties, IdempotentEventFilter idempotentEventFilter,
+      SchedulerHelper schedulerHelper, CounterMetric kafkaCounterMetric, TimerMetric kafkaTimerMetric,
+      KafkaUtil kafkaUtil, BalanceRepository balanceRepository, JsonHelper jsonHelper) {
+    super(new BaseSubscriberServiceBuilder()
+        .withKafkaProperties(kafkaProperties)
+        .withIdempotentEventFilter(idempotentEventFilter)
+        .withSchedulerHelper(schedulerHelper)
+        .withKafkaCounterMetric(kafkaCounterMetric)
+        .withTimerMetric(kafkaTimerMetric)
+        .withKafkaUtil(kafkaUtil)
+        .withEventHandler(new BalanceEventHandler(balanceRepository, jsonHelper))
+    );
   }
 
   @Override
@@ -42,17 +43,27 @@ public class CreateBalanceSubscriber extends SubscriberService {
     return KafkaTopic.CREATE_BALANCE;
   }
 
-  @Override
-  protected Mono<ReceiverRecord<String, String>> handleEvent(
-      ReceiverRecord<String, String> kafkaRecord) {
-    return balanceRepository.save(Balance.from(kafkaRecord, jsonHelper))
-        .map(result -> kafkaRecord);
+  @Component
+  public static class BalanceEventHandler implements EventHandler {
+
+    private final BalanceRepository balanceRepository;
+    private final JsonHelper jsonHelper;
+
+    public BalanceEventHandler(BalanceRepository balanceRepository, JsonHelper jsonHelper) {
+      this.balanceRepository = balanceRepository;
+      this.jsonHelper = jsonHelper;
+    }
+
+    @Override
+    public Mono<ReceiverRecord<String, String>> handle(EventType eventType,
+        ReceiverRecord<String, String> kafkaRecord) {
+      return switch (eventType) {
+        case ORIGINAL -> balanceRepository.save(Balance.from(kafkaRecord, jsonHelper))
+            .map(result -> kafkaRecord);
+        case DLT -> Mono.just(kafkaRecord)
+            .doOnNext(record -> log.warn("#CORE  - failed to consume create member event, {}", record.value()));
+      };
+    }
   }
 
-  @Override
-  protected Mono<ReceiverRecord<String, String>> handleDltEvent(
-      ReceiverRecord<String, String> dltRecord) {
-    return Mono.just(dltRecord)
-        .doOnNext(record -> log.warn("#CORE  - failed to consume create member event, {}", record.value()));
-  }
 }
